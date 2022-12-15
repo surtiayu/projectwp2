02d0cf6f4a3a5a574a9d147ce78559cadecce14d
surtiayu <surtiayu16@gmail.com>
2022-12-15 21:36:28 +0700

    upload project

---
+++ application/views/admin/_partials/footer.php
@@ -0,0 +1,8 @@
+<!-- Sticky Footer -->
+<footer class="sticky-footer">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright © <a href="https://www.sarjanakomedi.com">Khayangan Resort.2022</span>
+    </div>
+  </div>
+</footer>
---
+++ application/views/tamu/footer.php
@@ -0,0 +1,8 @@
+<!-- Sticky Footer -->
+<footer >
+  <div class="container mx-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright © <a href="">Kayangan Resort . 2022 </span>
+    </div>
+  </div>
+</footer>
